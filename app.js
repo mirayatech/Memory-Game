ee8d5b7b0facf29c183b86f3128b6f5b8215ef3b
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-05-25 21:15:39 +0200

    create match card function

--- app.js
+++ app.js
@@ -20,3 +20,35 @@ function flipCard(e) {
     matchCards(cardOneImg, cardTwoImg);
   }
 }
+
+// Function Match Card
+function matchCards(img1, img2) {
+  // if two card matches eachother
+  if (img1 === img2) {
+    matchedCard++; //increment matched value by 1
+    // if matched value is 8, user has matched all cards
+    if (matchedCard == 8) {
+      setTimeout(() => {
+        return shuffleCard();
+      }, 500); //clalling shuffelCard after 1sec
+    }
+    cardOne.removeEventListener("click", flipCard);
+    cardTwo.removeEventListener("click", flipCard);
+    cardOne = cardTwo = ""; //setting both card value to blank
+    return (disableDeck = false);
+  }
+  // if two card not matched
+  setTimeout(() => {
+    // adding shake class to both after 400ms
+    cardOne.classList.add("shake");
+    cardTwo.classList.add("shake");
+  }, 400);
+
+  setTimeout(() => {
+    // romoving shake class and flip it back after 1.2sec
+    cardOne.classList.remove("shake", "flip");
+    cardTwo.classList.remove("shake", "flip");
+    cardOne = cardTwo = ""; //setting both card value to blank
+    disableDeck = false;
+  }, 1200);
+}
